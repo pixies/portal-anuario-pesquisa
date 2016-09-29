9a97426dfd280a388f2adc9e4f121c6600cc56a8
Cleyton Fabio (pixies) <eu@cflb.co>
2016-09-29 14:06:18 -0300

    views pesquisador - editar, visualizar, deletar, listar

--- anuario/pesquisador/views.py
+++ anuario/pesquisador/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
+from .models import Pesquisador 
 
 # Create your views here.
+
+def todos_pesquisadores(request):
+	context = {}
+
+	context['pesquisadores'] = Pesquisador.objects.all()
+
+	return render (request, 'todos.html', context)
+
+def pesquisador(request, id_pesquisador):
+	pass
+
+def editar_pesquisador(request, id_pesquisador):
+	pass
+
+def remover_pesquisador(request, id_pesquisador):
+	pass
