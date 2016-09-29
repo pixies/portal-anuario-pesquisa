1bc13a7521b6397482e0e9e6a1fdd6e38889f610
Cleyton Fabio (pixies) <eu@cflb.co>
2016-09-29 12:08:50 -0300

    adcionando as rotas, core, accounts e static files

--- anuario/urls.py
+++ anuario/urls.py
@@ -15,8 +15,14 @@ Including another URLconf
 """
 from django.conf.urls import url, include
 from django.contrib import admin
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'', include('anuario.core.urls', namespace='anuario'))
+    url(r'', include('anuario.core.urls', namespace='core')),
+    url(r'^conta/', include('anuario.accounts.urls', namespace='accounts')),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
